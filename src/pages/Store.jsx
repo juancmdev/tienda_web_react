6e7c563a1b9c07a98ab4cbaaee95e94655df586a
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-10-22 15:19:05 -0500

    updated

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -3,6 +3,7 @@ import Buscador from "../components/Buscador";
 import products from "../data/products";
 import ProductCard from "../components/ProductCard";
 
+
 const Store = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -13,7 +14,7 @@ const Store = () => {
   return (
     <>
       <Buscador onBusquedaChange={setSearchTerm} />
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
         {filteredProducts.map((product, index) => (
           <ProductCard product={product} key={index} />
         ))}
