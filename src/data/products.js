b5d8d8ad1b44a0102a70259b52c7506a1ee491b0
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-10-22 12:48:56 -0500

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,22 @@
+const productos = [
+    {
+      id: '0001',
+      nombre: 'Bambú Zen 🎍',
+      precio: 18.50,
+      descripcion: 'Aroma fresco y relajante, ideal para el estudio o la oficina. Notas de té verde y sándalo.',
+      imagenUrl: '/images/bambu.jpg', // Ruta relativa a la carpeta 'public'
+      // EL ENLACE FIJO DE MERCADO PAGO PARA ESTE PRODUCTO:
+      linkPagoMP: '#', 
+    },
+    {
+      id: '0002',
+      nombre: 'Vainilla Suave 🍨',
+      precio: 14.99,
+      descripcion: 'Clásico y reconfortante aroma a vainilla de Madagascar. Perfecto para el hogar.',
+      imagenUrl: '/images/vainilla.jpg',
+      linkPagoMP: '#',
+    },
+    // ... Agrega más ambientadores
+  ];
+  
+  export default productos;
